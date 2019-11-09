ec2b9be45fb70f80191d1cc4b9072970d2b02bc9
Wisser <wisserwendelin@gmail.com>
2019-11-09 12:23:52 +0100

    Add record class mapping

---
+++ lib/record.dart
@@ -0,0 +1,19 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'record.g.dart';
+
+@JsonSerializable()
+class Record {
+  DateTime start;
+  DateTime end;
+  int breakMinutes;
+
+  Record(this.start, this.end, this.breakMinutes);
+
+  factory Record.fromMappedJson(Map<String, dynamic> json) =>
+      _$RecordFromJson(json);
+
+  Map<String, dynamic> toJson() => _$RecordToJson(this);
+
+  int calcTotalMinutes() {}
+}
---
+++ lib/record.g.dart
@@ -0,0 +1,20 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'record.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Record _$RecordFromJson(Map<String, dynamic> json) {
+  return Record(
+      json['start'] == null ? null : DateTime.parse(json['start'] as String),
+      json['end'] == null ? null : DateTime.parse(json['end'] as String),
+      json['breakMinutes'] as int);
+}
+
+Map<String, dynamic> _$RecordToJson(Record instance) => <String, dynamic>{
+      'start': instance.start?.toIso8601String(),
+      'end': instance.end?.toIso8601String(),
+      'breakMinutes': instance.breakMinutes
+    };
